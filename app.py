f1c81c9c03fd11306b5865817ec47cfe3794d181
heupelS <simeon@heupel.de>
2022-07-16 21:17:05 +0200

    Web-app complete

---
+++ app.py
@@ -0,0 +1,86 @@
+#app.py
+import re
+from turtle import title
+from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2 #pip install psycopg2 
+import psycopg2.extras
+ 
+app = Flask(__name__)
+app.secret_key = "cairocoders-ednalan"
+ 
+DB_HOST = "localhost"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "dbs_2022"
+ 
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+ 
+@app.route('/')
+def Index():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    s = "SELECT * FROM articles"
+    cur.execute(s) # Execute the SQL
+    list_articles = cur.fetchall()
+    return render_template('index.html', list_articles = list_articles)
+ 
+@app.route('/add_article', methods=['POST'])
+def add_article():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        year = request.form['year']
+        journal = request.form['journal']
+        h_index = request.form['h_index']
+        refperdoc = request.form['refperdoc']
+        cur.execute("INSERT INTO articles (id, author, title, year, journal, h_index, refperdoc) VALUES ((SELECT MAX(id)+1 FROM public.articles),%s,%s,%s,%s,%s,%s)", (author,title,year,journal,h_index,refperdoc))
+        conn.commit()
+        flash('article added')
+        return redirect(url_for('Index'))
+ 
+@app.route('/edit/<id>', methods = ['POST', 'GET'])
+def get_article(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   
+    cur.execute('SELECT * FROM articles WHERE id = %s', (id))
+    data = cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('edit.html', article = data[0])
+ 
+@app.route('/update/<id>', methods=['POST'])
+def update_article(id):
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        year = request.form['year']
+        journal = request.form['journal']
+        h_index = request.form['h_index']
+        refperdoc = request.form['refperdoc']
+         
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("""
+            UPDATE articles
+            SET author = %s,
+                title = %s,
+                year = %s,
+                journal = %s,
+                h_index = %s,
+                refperdoc = %s                
+            WHERE id = %s
+        """, (author,title,year,journal,h_index,refperdoc,id))
+        flash('article updated')
+        conn.commit()
+        return redirect(url_for('Index'))
+ 
+@app.route('/delete/<string:id>', methods = ['POST','GET'])
+def delete_article(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   
+    cur.execute('DELETE FROM articles WHERE id = {0}'.format(id))
+    conn.commit()
+    flash('article deleted')
+    return redirect(url_for('Index'))
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
